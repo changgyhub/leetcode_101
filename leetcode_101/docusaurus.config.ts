3d57f39f83dde643c192c5de3e52717b7433af40
Yan-Ying Liao <n9102125@gmail.com>
2024-11-11 21:40:35 +0100

    add github action

--- leetcode_101/docusaurus.config.ts
+++ leetcode_101/docusaurus.config.ts
@@ -51,7 +51,8 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/noworneverev/leetcode_101",
+          editUrl:
+            "https://github.com/noworneverev/leetcode_101/tree/master/leetcode_101",
         },
         blog: false,
         theme: {
